85caec42a8fb760763d9768d8956b58d6fae2c21
Rushikesh <rushikesh.mehta@agiliad.com>
2022-01-24 10:38:52 +0530

    commit2

---
+++ GITHUB2/src/Git/Git1.java
@@ -0,0 +1,19 @@
+package Git;
+
+public class Git1 {
+	  static int  no1=5;
+	  static int y=10;
+	 
+	public static void main(String[]args) {
+	 
+	 int result2=add();
+	 System.out.println(result2);
+	 }
+	static int add(){
+		int  result;
+		result=no1+y;
+		return result;
+	}
+}
+
+
